6528ae8d0a60d71bc1dfcd59bc5c04db43f67d06
itsoluwatobby <oluwasamuel050@gmail.com>
2022-11-09 10:14:34 +0100

    ready to deploy

--- src/axios/axiosFetch.jsx
+++ src/axios/axiosFetch.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BLOG_URL = 'https://blog-api-4cnd.onrender.com';
+const BLOG_URL = 'https://blog-api-4cnd.onrender.com/api/blog';
 
 export const axiosFetch = axios.create({
    baseURL: BLOG_URL,
